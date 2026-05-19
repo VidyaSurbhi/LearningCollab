e6e1e6e65a6a2f907fbe124d16ed00031dd1e495
Vidya Surbhi <75060192+VidyaSurbhi@users.noreply.github.com>
2026-05-19 08:32:22 +0530

    changes by viday 2 for second time at 19th  test

--- mailAdmin.py
+++ mailAdmin.py
@@ -8,3 +8,11 @@ Print("Vidya 1 changes at 19/05/2026")
 #changes made by vidya 2
 sum = 10 + 20
 print("Sum is ", sum)
+
+
+#changes made by vidya 1 second time 
+print("Vidya 1 changes second at 19/05/2026")
+
+#changes made by vidya 2 second time
+sub = 20- 10
+print(sub)
