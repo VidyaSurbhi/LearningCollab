598172b6bc288cbd89898b5883d1d46e95c3ba68
Vidya Surbhi <75060192+VidyaSurbhi@users.noreply.github.com>
2026-05-19 08:10:55 +0530

    changes by viday 1 second time at 19

--- mailAdmin.py
+++ mailAdmin.py
@@ -4,3 +4,10 @@ print("Admin File Loaded")
 print("changes maded to admin file by vidya 1")
 
 Print("Vidya 1 changes at 19/05/2026")
+
+#changes made by vidya 2
+sum = 10 + 20
+print("Sum is ", sum)
+
+#changes made by vidya 1 second time 
+print("Vidya 1 changes at 19/05/2026")
